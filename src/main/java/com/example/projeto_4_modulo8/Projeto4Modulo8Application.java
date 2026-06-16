8c9e43dbf925bb42a0155a4bb275119914494c61
gabriel augusto <gabriel.freitas3110@gmail.com>
2026-06-15 21:25:13 -0300

    feat: projeto criado

---
+++ src/main/java/com/example/projeto_4_modulo8/Projeto4Modulo8Application.java
@@ -0,0 +1,13 @@
+package com.example.projeto_4_modulo8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto4Modulo8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto4Modulo8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projeto_4_modulo8/Projeto4Modulo8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto_4_modulo8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto4Modulo8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

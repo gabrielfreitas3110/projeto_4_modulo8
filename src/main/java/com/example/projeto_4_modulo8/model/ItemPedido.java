afcc0ac25cf4ac6729abe29ecabd5088f5b30c4d
gabriel augusto <gabriel.freitas3110@gmail.com>
2026-07-08 20:20:20 -0300

    feat: adicionado ItemPedido

---
+++ src/main/java/com/example/projeto_4_modulo8/model/ItemPedido.java
@@ -0,0 +1,32 @@
+package com.example.projeto_4_modulo8.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ItemPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
+    private Produto produto;
+
+    private Integer quantidade;
+    private Double precoUnitario;
+    private Double subtotal;
+}
